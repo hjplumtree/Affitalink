41f46a4c640dea64727c1da53fd0830c4e928b10
Hee Jae <dark199174@gmail.com>
2022-03-27 22:40:21 +0900

    Add fetch advertisers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/cj-advertisers",
+        destination: `${process.env.NEXT_PUBLIC_CJ_ADVERTISERS_BASE_URL}?requestor-cid=${process.env.NEXT_PUBLIC_CJ_REQUESTOR_ID}&advertiser-ids=${process.env.NEXT_PUBLIC_CJ_ADVERTISER_IDS}`,
+      },
+      {
+        source: "/cj-offers",
+        destination: `${process.env.NEXT_PUBLIC_CJ_OFFERS_BASE_URL}?website-id=${process.env.NEXT_PUBLIC_CJ_WEBSITE_ID}&advertiser-ids=${process.env.NEXT_PUBLIC_CJ_ADVERTISER_IDS}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
