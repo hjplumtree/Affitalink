58bc6f859096969e67a2a68960fa227be8c13aba
Hee Jae <dark199174@gmail.com>
2022-04-06 14:56:08 +0900

    Add :: load link data

--- components/LinksList.js
+++ components/LinksList.js
@@ -1,4 +1,29 @@
-export default function LinksList(offers) {
-  console.log(offers);
-  return <div></div>;
+import { Flex, VStack, Box } from "@chakra-ui/react";
+
+export default function LinksList({ links }) {
+  const { $: meta, link: link_arr } = links;
+  console.log(meta, link_arr);
+  return (
+    <main>
+      {link_arr.map((link) => (
+        <Flex
+          key={link["link-id"]}
+          justifyContent="space-between"
+          p={5}
+          gap={2}
+        >
+          <VStack align="baseline" flex="2">
+            <Box>{link["advertiser-name"][0]}</Box>
+            <Box>{link["link-name"][0]}</Box>
+            <Box>{link["description"][0]}</Box>
+          </VStack>
+          <VStack align="baseline" flex="1">
+            <Box>{link["coupon-code"][0] ?? "No Need Code"}</Box>
+            <Box>{link["clickUrl"][0]}</Box>
+            <Box>{link["link-code-html"][0]}</Box>
+          </VStack>
+        </Flex>
+      ))}
+    </main>
+  );
 }
