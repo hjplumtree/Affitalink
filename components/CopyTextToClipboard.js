ba0b9c9222470c68f713004fed051113ccc89ac6
Hee Jae <dark199174@gmail.com>
2022-04-07 22:27:22 +0900

    Add :: copy text to clipboard when clicked

---
+++ components/CopyTextToClipboard.js
@@ -0,0 +1,37 @@
+import { Box, Text, Tooltip, useToast } from "@chakra-ui/react";
+
+export default function CopyTextToClipboard({ children }) {
+  const toast = useToast();
+
+  const handleClick = (e) => {
+    const text = e.target.firstChild.textContent;
+    navigator.clipboard.writeText(text);
+    toast({
+      title: "Copied successfully",
+      status: "info",
+      position: "top-right",
+      duration: 800,
+    });
+  };
+
+  return (
+    <Box
+      onClick={handleClick}
+      cursor="pointer"
+      p={1}
+      border="2px dashed #4895EF"
+      bg="#e1e1e1"
+    >
+      <Tooltip label="Click to copy">
+        <Text
+          width="170px"
+          whiteSpace="nowrap"
+          overflow="hidden"
+          textOverflow="ellipsis"
+        >
+          {children}
+        </Text>
+      </Tooltip>
+    </Box>
+  );
+}
--- components/LinksList.js
+++ components/LinksList.js
@@ -1,8 +1,8 @@
-import { Flex, VStack, Box } from "@chakra-ui/react";
+import { Flex, VStack, Box, Code, Heading, Text } from "@chakra-ui/react";
+import CopyTextToClipboard from "./CopyTextToClipboard";
 
 export default function LinksList({ links }) {
   const { $: meta, link: link_arr } = links;
-  console.log(meta, link_arr);
   return (
     <main>
       {link_arr &&
@@ -15,17 +15,23 @@ export default function LinksList({ links }) {
             wrap="wrap"
           >
             <VStack align="baseline" flexGrow="1" flexBasis="300px">
-              <Box>{link["advertiser-name"][0]}</Box>
-              <Box>{link["link-name"][0]}</Box>
-              <Box>{link["description"][0]}</Box>
+              <Code>{link["advertiser-name"][0]}</Code>
+              <Heading size="md">{link["link-name"][0]}</Heading>
+              <Text fontSize="sm">{link["description"][0]}</Text>
             </VStack>
             <VStack align="baseline" flex="0 0 100px">
-              <Box>
-                {link["coupon-code"][0].length === 0
-                  ? "No Code Needed"
-                  : `Code: ${link["coupon-code"][0]}`}
-              </Box>
-              <Box>{link["clickUrl"][0]}</Box>
+              {/* TODO */}
+              {/* Click to copy text*/}
+              {/*  */}
+
+              {link["coupon-code"][0].length === 0 ? (
+                <Box>No Code Needed</Box>
+              ) : (
+                <CopyTextToClipboard>
+                  ${link["coupon-code"][0]}
+                </CopyTextToClipboard>
+              )}
+              <CopyTextToClipboard>{link["clickUrl"][0]}</CopyTextToClipboard>
               <Box>{link["link-code-html"][0]}</Box>
             </VStack>
           </Flex>
