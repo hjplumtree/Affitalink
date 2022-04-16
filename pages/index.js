19bda2f8f3ecad79a61ae42d4fd15b880f6e4868
Hee Jae <dark199174@gmail.com>
2022-04-16 22:02:23 +0900

    Add :: temporary dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
 import SectionBox from "../components/SectionBox";
 import Header from "../components/Header";
+import { VStack, Heading, Divider } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <SectionBox>
-      <Header
-        title="Welcomde to Affitalink"
-        subtitle="It's beta service, feel free to use and contact me for feedback 😄"
-      />
-    </SectionBox>
+    <VStack>
+      <SectionBox>
+        <Header title="Welcome to Affitalink" />
+      </SectionBox>
+      <SectionBox>
+        <Heading size="lg" mb={7}>
+          Now is beta, feel free to use and feedback 😄
+        </Heading>
+        <Heading size="lg">1. Connect networks like CJ and Rakuten</Heading>
+        <Heading size="lg">2. Get tracking links</Heading>
+        <Heading size="lg">
+          Also find coupon code, description, image link ... etc
+        </Heading>
+      </SectionBox>
+    </VStack>
   );
 }
