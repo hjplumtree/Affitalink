e5043242441f895b34a2cace860aedcb17e88f6c
HeJ <dark199174@gmail.com>
2022-05-04 17:21:37 +0900

    Update :: button text

--- components/LinksHeader.js
+++ components/LinksHeader.js
@@ -98,7 +98,7 @@ export default function LinksHeader({
               fontSize="md"
               fontWeight="bold"
             >
-              Click here to connect
+              Click to connect
             </RouterLink>
           </Alert>
         </VStack>
