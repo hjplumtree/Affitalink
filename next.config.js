641525d3d4a1531f9c2a3ac6272e52aaf8b95ef6
HeJ <dark199174@gmail.com>
2022-05-04 15:15:50 +0900

    Fix :: unused rewrites comment out

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/cj-advertisers",
-        destination: `${process.env.NEXT_PUBLIC_CJ_ADVERTISERS_BASE_URL}`,
-      },
-      {
-        source: "/offers",
-        destination: `${process.env.NEXT_PUBLIC_CJ_OFFERS_BASE_URL}`,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/cj-advertisers",
+  //       destination: `${process.env.NEXT_PUBLIC_CJ_ADVERTISERS_BASE_URL}`,
+  //     },
+  //     {
+  //       source: "/offers",
+  //       destination: `${process.env.NEXT_PUBLIC_CJ_OFFERS_BASE_URL}`,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
