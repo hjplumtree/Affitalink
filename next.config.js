69944f65fff06e971624d4efd4f46fd1cc82096a
HeJ <dark199174@gmail.com>
2022-05-06 17:39:05 +0900

    Fix :: CORS error

--- next.config.js
+++ next.config.js
@@ -1,18 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/cj-advertisers",
-  //       destination: `${process.env.NEXT_PUBLIC_CJ_ADVERTISERS_BASE_URL}`,
-  //     },
-  //     {
-  //       source: "/offers",
-  //       destination: `${process.env.NEXT_PUBLIC_CJ_OFFERS_BASE_URL}`,
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/cj-advertisers",
+        destination: `https://advertiser-lookup.api.cj.com/v2/advertiser-lookup`,
+      },
+      {
+        source: "/cj-links",
+        destination: `https://link-search.api.cj.com/v2/link-search`,
+      },
+
+      // Doesn't work! Because POST??
+      {
+        source: "/rakuten-token",
+        destination: "https://api.linksynergy.com/token",
+      },
+      {
+        source: "/rakuten-links",
+        destination: "https://api.linksynergy.com/coupon/1.0",
+      },
+      {
+        source: "/rakuten-advertisers",
+        destination:
+          "https://api.linksynergy.com/linklocator/1.0/getMerchByAppStatus/approved",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
